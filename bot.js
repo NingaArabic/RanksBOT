a3aaef46ec4933649bfa4eed87d3bdc2f9e863d5
NingaArabic <43046589+NingaArabic@users.noreply.github.com>
2019-06-24 14:49:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "=";
-
+const fs = require("fs");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
